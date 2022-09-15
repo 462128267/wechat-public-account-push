b24811469bca164bc940fe3e5b0b77ae326e9d85
462128267 <113548115+462128267@users.noreply.github.com>
2022-09-15 10:09:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,20 +20,20 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: ["orUlF6nPJ-vbxP2yLdY351RzfzU8,orUlF6sqwxPU0tI3pO9hD0Zs7fLI"]
+  USERS: [
     {
       // 想要发送的人的名字
-      name: "谭理雪0",
+      name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-
+      id: "orUlF6nPJ-vbxP2yLdY351RzfzU8",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "kC-wpQqgsucbws3-65uP-YrzuZLeNLKHfxUZSZnCkD8",
       // 所在省份
-      province: "重庆",
+      province: "广东",
       // 所在城市
-      city: "重庆",
+      city: "肇庆",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '7-24',
+      horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -41,7 +41,7 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "谭理雪", "year": "2004", "date": "7-24"},
+        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
@@ -58,7 +58,7 @@ export const config = {
       ]
     },
     {
-      name: "谭理雪1",
+      name: "老婆1",
       id: "",
       useTemplateId: "",
       province: "",
@@ -70,7 +70,7 @@ export const config = {
       customizedDateList: []
     },
     {
-      name: "谭理雪2",
+      name: "老婆2",
       id: "",
       useTemplateId: "",
       province: "",
@@ -82,7 +82,7 @@ export const config = {
       customizedDateList: []
     },
     {
-      name: "谭理雪3",
+      name: "老婆3",
       id: "",
       useTemplateId: "",
       province: "",
@@ -102,12 +102,12 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "	MrAr1aJEayBVB_ctHBT6pnH24nW5_8yIdBCEzi3boH0",
+  CALLBACK_TEMPLATE_ID: "	MrAr1aJEayBVB_ctHBT6pnH24nW5_8yIdBCEzi3boH0	",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-  CALLBACK_USERS: ["orUlF6nPJ-vbxP2yLdY351RzfzU8"]
+  CALLBACK_USERS: [
     {
-      // 一般都填自己orUlF6nPJ-vbxP2yLdY351RzfzU8
+      // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "orUlF6nPJ-vbxP2yLdY351RzfzU8",
@@ -123,9 +123,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "重庆",
+  PROVINCE: "广东",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "重庆",
+  CITY: "惠州",
 
   /** 重要节日相关 */
 
